5f2a2bd61d721b62b17a43d6fa9930f2ddd6bb2f
Junyoung Clare Jang <jjc9310@gmail.com>
2018-02-01 07:35:12 -0500

    chore(scripts): update test script
    Original script does not run tests recursively.

--- scripts/test.js
+++ scripts/test.js
@@ -14,9 +14,23 @@ const mocha = new Mocha({
 const cwd = process.cwd()
 const testsDir = path.join(cwd, 'tests')
 
-fs.readdirSync(testsDir)
-  .filter((file) => file.match(/\.test\.js$/))
-  .forEach((file) => mocha.addFile(path.join(testsDir, file)))
+const registerDir = (dir) => {
+  const entries = fs.readdirSync(dir)
+     .map((fileName) => path.join(dir, fileName))
+
+  entries
+    .filter((filePath) => filePath.match(/\.test\.js$/))
+    .forEach((filePath) => mocha.addFile(filePath))
+
+  entries
+    .filter((filePath) => fs.statSync(filePath).isDirectory())
+    .forEach(filePath => {
+      console.log(filePath)
+      registerDir(filePath)
+    })
+}
+
+registerDir(testsDir)
 
 mocha.run((failures) => {
   process.on('exit', function () {
