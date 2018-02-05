0feb3cee52de493a6b8ad10be16254c12e2909bc
Junyoung Clare Jang <jjc9310@gmail.com>
2018-02-04 23:53:22 -0500

    fix(typings): add a type for periodOf function

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -4,6 +4,7 @@ import { Scheduler } from 'rxjs/Scheduler'
 declare namespace Rxan {
   const msElasped: (scheduler?: Scheduler) => Observable<number>
   const during: (scheduler?: Scheduler) => (duration: number) => Observable<number>
+  const periodOf: (scheduler?: Scheduler) => (period: number, cycles?: number) => Observable<number>
 
   module easing {
     interface EasingFunc {
